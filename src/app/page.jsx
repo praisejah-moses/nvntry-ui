a5ab21c028d9e01399fd308d3626c008d029b41f
Osarodion Douglas Idumwonyi <98432474+lazydoug@users.noreply.github.com>
2024-05-09 14:55:20 +0100

    fix svg rendering

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,10 +5,10 @@ const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
 const Home = () => {
   return (
-    <main className={`relative ${spaceGrotesk.className}`}>
-      <div className='absolute w-full h-full bg-[url("https://res.cloudinary.com/drgfavmlw/image/upload/v1715205817/Spiral-Chaos_wexgby.svg")] bg-no-repeat bg-cover bg-center animate-[pulse_5s_ease-in-out_infinite]'></div>
-      <div className='min-h-fit md:min-h-screen flex items-center -webkit-backdrop-blur-16'>
-        <section className='w-full bg-gradient-to-b from-white to-[#EBEBEB] py-10 mx-auto md:rounded-3xl sm:w-[512px] sm:drop-shadow-[0_35px_35px_rgba(0,0,0,1)]'>
+    <main className={`relative min-w-fit ${spaceGrotesk.className}`}>
+      <div className='absolute w-full h-full bg-[url("https://res.cloudinary.com/drgfavmlw/image/upload/v1715205817/Spiral-Chaos_wexgby.svg")] bg-no-repeat bg-cover bg-center md:animate-[pulse_5s_ease-in-out_infinite]'></div>
+      <div className='h-screen md:flex md:items-center -webkit-backdrop-blur-16'>
+        <section className='w-full h-full bg-gradient-to-b from-white to-[#EBEBEB] py-10 mx-auto md:rounded-3xl md:w-[512px] md:drop-shadow-[0_35px_35px_rgba(0,0,0,1)]'>
           <div className='px-9'>
             <div className='flex items-center justify-between'>
               <div className='w-9/12'>
@@ -50,86 +50,22 @@ const Home = () => {
             <div className='relative w-64 h-96 md:hidden left-1/2 -translate-x-1/2 mt-10'>
               <Image src='/assets/product.png' alt='Product image' fill />
 
-              {/* <svg
-                className='absolute z-20 -bottom-10 left-1/2 -translate-x-1/2 -webkit-backdrop-blur-12 shape'
-                width='209'
-                height='93'
-                viewBox='0 0 209 93'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'>
-                <g filter='url(#filter0_b_4_1327)'>
-                  <path
-                    d='M2.5543 23.755C1.19008 11.0677 11.1315 0 23.8919 0H185.108C197.868 0 207.81 11.0677 206.446 23.755L201.061 73.8337C199.889 84.7352 190.688 93 179.723 93H29.2768C18.3125 93 9.11131 84.7352 7.93911 73.8337L2.5543 23.755Z'
-                    fill='url(#paint0_linear_4_1327)'
-                    fillOpacity='0.6'
-                  />
-                  <path
-                    d='M23.8919 0.5H185.108C197.571 0.5 207.281 11.3099 205.949 23.7016L200.564 73.7803C199.419 84.4277 190.432 92.5 179.723 92.5H29.2768C18.5679 92.5 9.58113 84.4277 8.43624 73.7803L3.05144 23.7015C1.719 11.3099 11.4288 0.5 23.8919 0.5Z'
-                    stroke='url(#paint1_linear_4_1327)'
-                  />
-                </g>
-                <defs>
-                  <filter
-                    id='filter0_b_4_1327'
-                    x='-21.5711'
-                    y='-24'
-                    width='252.142'
-                    height='141'
-                    filterUnits='userSpaceOnUse'
-                    colorInterpolationFilters='sRGB'>
-                    <feFlood floodOpacity='0' result='BackgroundImageFix' />
-                    <feGaussianBlur in='BackgroundImageFix' stdDeviation='12' />
-                    <feComposite
-                      in2='SourceAlpha'
-                      operator='in'
-                      result='effect1_backgroundBlur_4_1327'
-                    />
-                    <feBlend
-                      mode='normal'
-                      in='SourceGraphic'
-                      in2='effect1_backgroundBlur_4_1327'
-                      result='shape'
-                    />
-                  </filter>
-                  <linearGradient
-                    id='paint0_linear_4_1327'
-                    x1='105'
-                    y1='93'
-                    x2='79.1787'
-                    y2='-145.311'
-                    gradientUnits='userSpaceOnUse'>
-                    <stop stopColor='white' />
-                    <stop offset='1' stopColor='white' stopOpacity='0.53' />
-                  </linearGradient>
-                  <linearGradient
-                    id='paint1_linear_4_1327'
-                    x1='104.5'
-                    y1='0'
-                    x2='104.5'
-                    y2='93'
-                    gradientUnits='userSpaceOnUse'>
-                    <stop stopColor='white' />
-                    <stop offset='1' stopColor='white' stopOpacity='0.26' />
-                  </linearGradient>
-                </defs>
-              </svg> */}
-
               <svg
                 className='absolute z-20 -bottom-10 left-1/2 -translate-x-1/2 -webkit-backdrop-blur-12 shape'
-                width='205'
+                width='204'
                 height='93'
                 viewBox='0 0 205 93'
                 fill='none'
                 xmlns='http://www.w3.org/2000/svg'>
                 <path
                   d='M21.8919 0.5H183.108C195.571 0.5 205.281 11.3099 203.949 23.7016L198.564 73.7803C197.419 84.4277 188.432 92.5 177.723 92.5H27.2768C16.5679 92.5 7.58113 84.4277 6.43624 73.7803L1.05144 23.7015C-0.281002 11.3099 9.42885 0.5 21.8919 0.5Z'
-                  fill='url(#paint0_linear_4_1328)'
-                  fill-opacity='0.6'
-                  stroke='url(#paint1_linear_4_1328)'
+                  fill='url(#paint0_linear_271_150)'
+                  fillOpacity='0.6'
+                  stroke='url(#paint1_linear_271_150)'
                 />
                 <defs>
                   <linearGradient
-                    id='paint0_linear_4_1328'
+                    id='paint0_linear_271_150'
                     x1='103'
                     y1='93'
                     x2='77.1787'
@@ -139,14 +75,14 @@ const Home = () => {
                     <stop offset='1' stopColor='white' stopOpacity='0.53' />
                   </linearGradient>
                   <linearGradient
-                    id='paint1_linear_4_1328'
+                    id='paint1_linear_271_150'
                     x1='102.5'
                     y1='0'
                     x2='102.5'
                     y2='93'
                     gradientUnits='userSpaceOnUse'>
                     <stop stopColor='white' />
-                    <stop offset='1' stopColor='white' stop-Opacity='0.26' />
+                    <stop offset='1' stopColor='white' stopOpacity='0.26' />
                   </linearGradient>
                 </defs>
               </svg>
