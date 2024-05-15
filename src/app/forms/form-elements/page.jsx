392b9b944bcac1b67583d359bb2b5d49183df631
Osarodion Douglas Idumwonyi <98432474+lazydoug@users.noreply.github.com>
2024-05-15 22:01:42 +0100

    admin dashboard trial run

---
+++ src/app/forms/form-elements/page.jsx
@@ -0,0 +1,18 @@
+import FormElements from '@/components/FormElements'
+import DefaultLayout from '@/components/DefaultLayout'
+
+export const metadata = {
+  title: 'Next.js Form Elements | TailAdmin - Next.js Dashboard Template',
+  description:
+    'This is Next.js Form Elements page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template',
+}
+
+const FormElementsPage = () => {
+  return (
+    <DefaultLayout>
+      <FormElements />
+    </DefaultLayout>
+  )
+}
+
+export default FormElementsPage
